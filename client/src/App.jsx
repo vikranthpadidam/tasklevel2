0d0bfcf668866338446d8442f2b81b15cba49d45
vikranthpadidam <vikranthpadidam@gmail.com>
2023-07-16 18:36:00 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import Login from './login.jsx';
+import Signup from './signup.jsx';
+
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Dashboard from './Dashboard';
+import Home from './Home';
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App;
